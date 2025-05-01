51cb76e7ffcdc45e2596e80bc0ef82d67b089f81
KingDonutOfAllDonuts <sirdonut12345@gmail.com>
2025-04-30 23:25:42 -0500

    initialized website, pages, tailwind

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex justify-center w-screen">
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/catering.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex justify-center w-screen">
+    <h1 class="text-xl">This is the catering page</h1>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
